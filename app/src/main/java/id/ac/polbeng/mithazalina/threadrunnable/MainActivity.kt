c3d1c7f92567d18a6f50278f323dcc2ba5dcbeb5
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-10-29 08:57:27 +0700

    push project

---
+++ app/src/main/java/id/ac/polbeng/mithazalina/threadrunnable/MainActivity.kt
@@ -0,0 +1,31 @@
+package id.ac.polbeng.mithazalina.threadrunnable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.polbeng.mithazalina.threadrunnable.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Thread {
+                killSomeTime()
+            }.start()
+        }
+    }
+
+    private fun killSomeTime() {
+        for (i in 1..20) {
+            runOnUiThread {
+                binding.textView.text = i.toString()
+            }
+            Thread.sleep(2000)
+            println("i: $i")
+        }
+    }
+}
